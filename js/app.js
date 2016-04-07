2fb938e09cd3a8439ef12f3f00fa18d2748b658b
Shaunak Sen <shaunak1105@gmail.com>
2016-04-07 10:04:33 +0530

    Added form validation

--- js/app.js
+++ js/app.js
@@ -1,8 +1,8 @@
 /**
  * Created by root on 2/4/16.
  */
-var myApp  = angular.module('myApp',[]);
-myApp.controller('mainController', function($scope, $log){
+var myApp  = angular.module('myApp',['ngMessages','ngResource']);
+myApp.controller('mainController', function($scope, $log, $filter,$resource){
     $scope.name="Mini Sen";
     $scope.eats = "chocolates";
     $scope.getName = function()
@@ -10,6 +10,20 @@ myApp.controller('mainController', function($scope, $log){
         return this.name;
     };
     console.log($log);
-    console.log($scope.getName());
-    console.log($scope);
+    //console.log($scope.getName());
+    //console.log($scope);
+    $scope.formattedName = $filter('uppercase')($scope.name);
+    $log.info($scope.formattedName);
+    $log.info($resource);
+
+
 });
+var searchPeople = function searchPeople($scope,fName,lName,ht,age){
+    return 'MINI MINI';
+};
+var searchPeopleString = searchPeople.toString();
+
+console.log(searchPeopleString);
+//returns the entire function as a string
+
+console.log(angular.injector().annotate(searchPeople));
