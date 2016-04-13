191aa2ef9c0478374195c34fcac84f16426f22e7
Shaunak Sen <shaunak1105@gmail.com>
2016-04-13 17:13:53 +0530

    used  GET

--- js/app3.js
+++ js/app3.js
@@ -1,16 +1,31 @@
 var myApp  = angular.module('myApp',['ngMessages','ngResource']);
 
-myApp.controller('mainController',['$scope','$log','$timeout','$filter',function($scope,$log,$timeout,$filter){
-    $scope.handle = ""
+myApp.controller('mainController',['$scope','$log','$timeout','$filter','$http',function($scope,$log,$timeout,$filter,$http){
+    $scope.handle = "";
     $scope.characters = 5;
-    var display_message= function(){
-        return $scope.handle.length === 5;
+
+
+    //native javascript ajax
+    /*
+    var rulesrequest = new XMLHttpRequest();
+    rulesrequest.onreadystatechange= function () {
+        $scope.$apply(function(){
+            if(rulesrequest.readyState==4 && rulesrequest.status==200){
+                console.log(JSON.parse(rulesrequest.responseText));
+                $scope.rules=JSON.parse(rulesrequest.responseText);
+            }
+        });
+
     };
-    $scope.rules = [
-        {ruleName: "Must be 5 characters"},
-        {ruleName: "Must not exist elsewhere"},
-        {ruleName: "Must be cool"}
-    ];
+    rulesrequest.open("GET","https://api.myjson.com/bins/3ay8z",true);
+    rulesrequest.send();
+    */
 
-
+    $http.get('https://api.myjson.com/bins/3ay8z')
+        .success(function(result){
+            $scope.rules=result;
+        })
+        .error(function (data,status) {
+            console.log(status);
+        });
 }]);
