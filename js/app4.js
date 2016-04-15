0b4989ee2114a88f5c62b661b2de5ba86a8781b8
Shaunak Sen <shaunak1105@gmail.com>
2016-04-16 00:41:25 +0530

    created my own service

--- js/app4.js
+++ js/app4.js
@@ -17,12 +17,27 @@ myApp.config(function($routeProvider){
         })
 });
 
-myApp.controller('mainController',['$scope','$log','$location',function($scope,$log,$location){
+myApp.service('nameService',function(){
+    var self = this;
+    this.name="Mini Sen";
+    this.namelength = function(){
+        return self.name.length;
+    }
+});
+
+myApp.controller('mainController',['$scope','$log','$location','nameService',function($scope,$log,$location,nameService){
     console.log($location.path());
-    $scope.name = "MINI";
+    $scope.$watch('name',function(){
+        nameService.name = $scope.name;
+        //remember nameService is a singleton...In this controller we set value of nameService.name so in the second page also this value is called
+    });
+    $log.log(nameService.name);
+    $log.log(nameService.namelength());
 }]);
 
-myApp.controller('secondController',['$scope','$log','$routeParams',function($scope,$log,$routeParams){
-    $scope.name="SHONA";
+myApp.controller('secondController',['$scope','$log','$routeParams','nameService',function($scope,$log,$routeParams,nameService){
+    $scope.name=nameService.name;
     $scope.num = $routeParams.num || 1;
+    $log.log(nameService.name);
+    $log.log(nameService.namelength());
 }]);
