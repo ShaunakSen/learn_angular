bba9a11be2e2fa3b83b5b683400be1c68e647cc2
Shaunak Sen <shaunak1105@gmail.com>
2016-05-14 14:37:15 +0530

    understood @

--- js/app5.js
+++ js/app5.js
@@ -1,33 +1,40 @@
-var myApp  = angular.module('myApp',['ngRoute']);
+var myApp = angular.module('myApp', ['ngRoute']);
 
 
-myApp.config(function($routeProvider){
+myApp.config(function ($routeProvider) {
     $routeProvider
-        .when('/',{
-            templateUrl:'index3.html',
-            controller:'mainController'
+        .when('/', {
+            templateUrl: 'index3.html',
+            controller: 'mainController'
         })
-        .when('/second',{
-            templateUrl:'index3_second.html',
-            controller:'secondController'
+        .when('/second', {
+            templateUrl: 'index3_second.html',
+            controller: 'secondController'
         });
 });
 
-myApp.controller('mainController',['$scope','$log','$location','nameService',function($scope,$log,$location,nameService){
-
-}]);
-
-myApp.controller('secondController',['$scope','$log','$routeParams','nameService',function($scope,$log,$routeParams,nameService){
-
-}]);
-
-myApp.directive("searchResult",function(){
+myApp.directive("searchResult", function () {
     return {
-        restrict: 'AECM',
-        template: '<a href="#" class="list-group-item">' +
-        '<h4 class="list-group-item-heading">Shaunak Sen</h4>' +
-        '<p class="list-group-item-text">89 Dum Dum Park</p> ' +
-        '</a>',
-        replace: true
+        restrict: 'AEM',
+        templateUrl: 'directives/searchresult.html',
+        replace: true,
+        scope: {
+            personName: "@",
+            personAddress: "@"
+        }
     };
 });
+
+myApp.controller('mainController', ['$scope', '$log', '$location', function ($scope, $log, $location) {
+    $scope.person = {
+        name: 'Buddhu Mini',
+        address: '3/1 Badabazar Jhansi 284002'
+    };
+
+}]);
+
+myApp.controller('secondController', ['$scope', '$log', '$routeParams', 'nameService', function ($scope, $log, $routeParams, nameService) {
+
+}]);
+
+
