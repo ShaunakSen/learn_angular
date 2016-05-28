92b52dcd30a7be060c5b5dbe92fe88fc05b7705b
Shaunak Sen <shaunak1105@gmail.com>
2016-05-28 14:19:56 +0530

    added filter app

--- project2/js/script6.js
+++ project2/js/script6.js
@@ -1,7 +1,36 @@
-var myApp = angular.module("myModule", []);
+var myApp = angular.module("myModule", ["ngRoute"]);
+myApp.config(function ($routeProvider) {
+    $routeProvider
+        .when(
+        "/home", {
+            templateUrl: "templates/home.html",
+            controller: "homeController"
+        })
+        .when(
+        "/courses", {
+            templateUrl: "templates/courses.html",
+            controller: "coursesController"
+        })
+        .when(
+        "/students", {
+            templateUrl: "templates/students.html",
+            controller: "studentsController"
+        })
+});
 
 
-myApp.controller("myController", function ($scope, $location, $http, $anchorScroll) {
+myApp.controller("homeController", function ($scope) {
+    $scope.message = "Home Page"
+});
 
+myApp.controller("coursesController", function ($scope) {
+    $scope.courses = ["Meteor", "React & Redux", "Angular", "Django", "Laravel"];
+});
+
+myApp.controller("studentsController", function ($scope, $http) {
+    $http.get("http://localhost/series/webservice/webservice.php")
+        .then(function(response){
+            $scope.students = response.data;
+        });
 });
 
