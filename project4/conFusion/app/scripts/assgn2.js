9a9e8b1d20935ca83c1de48e9831704539d88b75
Shaunak Sen <shaunak1105@gmail.com>
2016-06-25 16:29:25 +0530

    Fixed textarea bug

--- project4/conFusion/app/scripts/assgn2.js
+++ project4/conFusion/app/scripts/assgn2.js
@@ -1,53 +1,53 @@
-var app = angular.module('confusionApp',[]);
+var app = angular.module('confusionApp', []);
 
-app.controller('dishDetailController', function() {
+app.controller('dishDetailController', function () {
     this.search = '';
 
-    var dish={
-        name:'Uthapizza',
+    var dish = {
+        name: 'Uthapizza',
         image: 'images/uthapizza.png',
         category: 'mains',
-        label:'Hot',
-        price:'4.99',
-        description:'A unique combination of Indian Uthappam (pancake) and Italian pizza, topped with Cerignola olives, ripe vine cherry tomatoes, Vidalia onion, Guntur chillies and Buffalo Paneer.',
+        label: 'Hot',
+        price: '4.99',
+        description: 'A unique combination of Indian Uthappam (pancake) and Italian pizza, topped with Cerignola olives, ripe vine cherry tomatoes, Vidalia onion, Guntur chillies and Buffalo Paneer.',
         comments: [
             {
-                rating:5,
-                comment:"Imagine all the eatables, living in conFusion!",
-                author:"John Lemon",
-                date:"2012-10-16T17:57:28.556094Z"
+                rating: 5,
+                comment: "Imagine all the eatables, living in conFusion!",
+                author: "John Lemon",
+                date: "2012-10-16T17:57:28.556094Z"
             },
             {
-                rating:4,
-                comment:"Sends anyone to heaven, I wish I could get my mother-in-law to eat it!",
-                author:"Paul McVites",
-                date:"2014-09-05T17:57:28.556094Z"
+                rating: 4,
+                comment: "Sends anyone to heaven, I wish I could get my mother-in-law to eat it!",
+                author: "Paul McVites",
+                date: "2014-09-05T17:57:28.556094Z"
             },
             {
-                rating:3,
-                comment:"Eat it, just eat it!",
-                author:"Michael Jaikishan",
-                date:"2015-02-13T17:57:28.556094Z"
+                rating: 3,
+                comment: "Eat it, just eat it!",
+                author: "Michael Jaikishan",
+                date: "2015-02-13T17:57:28.556094Z"
             },
             {
-                rating:4,
-                comment:"Ultimate, Reaching for the stars!",
-                author:"Ringo Starry",
-                date:"2013-12-02T17:57:28.556094Z"
+                rating: 4,
+                comment: "Ultimate, Reaching for the stars!",
+                author: "Ringo Starry",
+                date: "2013-12-02T17:57:28.556094Z"
             },
             {
-                rating:2,
-                comment:"It's your birthday, we're gonna party!",
-                author:"25 Cent",
-                date:"2011-12-02T17:57:28.556094Z"
+                rating: 2,
+                comment: "It's your birthday, we're gonna party!",
+                author: "25 Cent",
+                date: "2011-12-02T17:57:28.556094Z"
             }
 
         ]
     };
 
-    function inArray(array, item){
-        for(var i=0; i<array.length; ++i){
-            if(array[i] === item){
+    function inArray(array, item) {
+        for (var i = 0; i < array.length; ++i) {
+            if (array[i] === item) {
                 return true;
             }
         }
@@ -55,29 +55,43 @@ app.controller('dishDetailController', function() {
     }
 
     this.dish = dish;
-    this.order = function(param){
+    this.order = function (param) {
         orderString = '';
-        var acceptable2 = ['-rating','-date','-author','-comment'];
-        var acceptable = ['rating','date','author','comment'];
-        if(param.charAt(0) === '-'){
-            if(inArray(acceptable2, param)){
+        var acceptable2 = ['-rating', '-date', '-author', '-comment'];
+        var acceptable = ['rating', 'date', 'author', 'comment'];
+        if (param.charAt(0) === '-') {
+            if (inArray(acceptable2, param)) {
                 return param;
             }
-            else{
+            else {
                 return '';
             }
         }
-        else{
-            if(inArray(acceptable, param)){
+        else {
+            if (inArray(acceptable, param)) {
                 return param;
             }
-            else{
+            else {
                 return '';
             }
         }
     }
 });
 
-app.controller('CommentFormController',['$scope', function($scope){
+app.controller('CommentFormController', ['$scope', function ($scope) {
+
+    //SETTING DEFAULT VALUES
+    $scope.comments = {
+        name: "",
+        ratings: 5,
+        comment: ""
+    };
+
+    $scope.validateTextarea = function(){
+        console.log('here');
+        console.log($scope.commentsForm);
+        return $scope.comments['comment'] === "" && !$scope.commentsForm.comment.$pristine;
+
+    }
 
 }]);
